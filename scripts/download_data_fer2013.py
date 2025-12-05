89a735eec293ee90b3722defddbd016bbe1a4353
timur-feix <vfdcz87d2d@privaterelay.appleid.com>
2025-12-05 21:56:36 +0100

    finished downloading script

--- scripts/download_data_fer2013.py
+++ scripts/download_data_fer2013.py
@@ -2,4 +2,16 @@ import kagglehub
 from pathlib import Path
 import shutil
 
-download_path = kagglehub.dataset_download("msambare/fer2013")
+target_path = Path("./data/raw")
+preparation_processed_path = Path("./data/processed")
+
+# download data from kaggle and retrieve destination
+download_path = Path(kagglehub.dataset_download("msambare/fer2013"))
+
+# copy the data into target path (./data/raw), also create (./data/processed)
+target_path.mkdir(parents=True, exist_ok=True)
+preparation_processed_path.mkdir(parents=True, exist_ok=True)
+shutil.copytree(download_path, target_path, dirs_exist_ok=True)
+
+# delete original downloaded data
+shutil.rmtree(download_path)
